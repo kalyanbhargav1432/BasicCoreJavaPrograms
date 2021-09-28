9415c9085b94078e59fa16dbfb7abcfcefbefea0
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-29 00:11:24 +0530

    Refactored: to print the 2D_Arrays in matrix form

--- Arrays.java
+++ Arrays.java
@@ -1,26 +1,28 @@
 package Assignment_3;
+
 import java.util.Scanner;
+
 public class Arrays {
-public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
-System.out.println("Enter Rows :");
-int Rows = scanner.nextInt();
-System.out.println("Enter Column :");
-int Columns = scanner.nextInt();
-int Array [][] = new int[Rows][Columns];
-for(int i = 0; i < Rows; i++) {
-	System.out.println(i + 1 +"Row data ");
-	for(int j = 0; j < Columns; j++) {
-		System.out.println(j + 1 +"Column data ");
-		Array [i][j] = scanner.nextInt();
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Rows :");
+		int Rows = scanner.nextInt();
+		System.out.println("Enter Column :");
+		int Columns = scanner.nextInt();
+		int Array[][] = new int[Rows][Columns];
+		for (int i = 0; i < Rows; i++) {
+			System.out.println(i + 1 + ":Row data ");
+			for (int j = 0; j < Columns; j++) {
+				System.out.println(j + 1 + ":Column data ");
+				Array[i][j] = scanner.nextInt();
+			}
+		}
+		System.out.println(" Matrix form: ");
+		for (int i = 0; i < Rows; i++) {
+			for (int j = 0; j < Columns; j++) {
+				System.out.print("  " + Array[i][j]);
+			}
+			System.out.println(" ");
+		}
 	}
-}
-System.out.println(" Matrix form: ");
-for(int i = 0; i < Rows; i++) {
-	for(int j = 0; j < Columns; j++) {
-		System.out.print("  "+Array[i][j]);
-	}
-	System.out.println(" ");
-}
-}
 }
