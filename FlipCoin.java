59123e3314c93846ced9f0b683561f34eede69f6
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-28 20:29:12 +0530

    Added: Number of times flip the coin with persentage

---
+++ FlipCoin.java
@@ -0,0 +1,28 @@
+package Assignment_3;
+
+import java.util.Scanner;
+
+public class FlipCoin {
+	public static void main(String[] args) {
+		int Heads = 0;
+		int Tails = 1;
+		int count = 0;
+		Scanner flipcoin = new Scanner(System.in);
+		System.out.println("Number of tims coin flipping:");
+		int NumberOfFlips = flipcoin.nextInt();
+		while (count != NumberOfFlips) {
+			count++;
+			int random = (int) Math.floor(Math.random() * 2);
+			if (random == 0) {
+				Tails++;
+			} else
+				Heads++;
+		}
+		System.out.println("Number of times Heads comes:" + Heads);
+		System.out.println("Number of times Tails comes:" + Tails);
+		double TailsPercentage = (double) Tails / NumberOfFlips * 100;
+		double HeadsPercentage = (double) Heads / NumberOfFlips * 100;
+		System.out.println(" TailsPercentage is:" + TailsPercentage);
+		System.out.println(" HeadsPercentage is:" + HeadsPercentage);
+	}
+}
