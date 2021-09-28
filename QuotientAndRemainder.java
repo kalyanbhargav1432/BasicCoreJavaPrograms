1f9f6f933c3ae01e488fc8880f344ef52d22f730
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-28 16:28:59 +0530

    Refactored: To print the Quotient and Remainder

--- QuotientAndRemainder.java
+++ QuotientAndRemainder.java
@@ -1,12 +1,12 @@
 package Assignment_3;
 
 public class QuotientAndRemainder {
-public static void main(String[] args) {
-int dividend = 15, divisor = 4;
-int Quotient = dividend / divisor;
-int Remainder = dividend % divisor;
-System.out.println("The Quotient is:" + Quotient);
-System.out.println("The Remainder is:" + Remainder);
+	public static void main(String[] args) {
+		int dividend = 23, divisor = 4;
+		int Quotient = dividend / divisor;
+		int Remainder = dividend % divisor;
+		System.out.println("The Quotient is:" + Quotient);
+		System.out.println("The Remainder is:" + Remainder);
 
-}
+	}
 }
