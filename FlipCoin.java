4a9f16061b354d8347d13f7ec8dd2aff8315dd1c
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-29 21:28:38 +0530

    Refactored: functionality to check number of times fliping and percentage

--- FlipCoin.java
+++ FlipCoin.java
@@ -4,24 +4,26 @@ import java.util.Scanner;
 
 public class FlipCoin {
 	public static void main(String[] args) {
-		int Heads = 0;
-		int Tails = 1;
+		final int HEADS = 1;
+		final int TAILS = 0;
+		int headscount = 0;
+		int tailscount = 0;
 		int count = 1;
 		Scanner flipcoin = new Scanner(System.in);
 		System.out.println("Number of tims coin flipping:");
 		int NumberOfFlips = flipcoin.nextInt();
-		while (count != NumberOfFlips) {
+		while (count <= NumberOfFlips) {
 			count++;
 			int random = (int) Math.floor(Math.random() * 2);
-			if (random == 0) {
-				Tails++;
-			} else
-				Heads++;
+			if (random == TAILS) {
+				tailscount++;
+			} else if (random == HEADS)
+				headscount++;
 		}
-		System.out.println("Number of times Heads comes:" + Heads);
-		System.out.println("Number of times Tails comes:" + Tails);
-		double TailsPercentage = (double) Tails / NumberOfFlips * 100;
-		double HeadsPercentage = (double) Heads / NumberOfFlips * 100;
+		System.out.println("Number of times Heads comes:" + headscount);
+		System.out.println("Number of times Tails comes:" + tailscount);
+		double TailsPercentage = (double) tailscount / NumberOfFlips * 100;
+		double HeadsPercentage = (double) headscount / NumberOfFlips * 100;
 		System.out.println(" TailsPercentage is:" + TailsPercentage);
 		System.out.println(" HeadsPercentage is:" + HeadsPercentage);
 	}
