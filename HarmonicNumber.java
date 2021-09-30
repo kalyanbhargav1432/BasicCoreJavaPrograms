f0ad36e8db499fed7227f487b1ad6ca1bdb1775a
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-30 17:03:33 +0530

    Added: Functionality to check the Harmonic Number

--- HarmonicNumber.java
+++ HarmonicNumber.java
@@ -1,15 +1,14 @@
 package Assignment_3;
 
-import java.io.*;
-
 public class HarmonicNumber {
 	static double nthHarmonic(int n) {
 		double number = 1;
 		for (int i = 2; i <= n; i++) {
-			number = number + (double)1 / i;
+			number = number + (double) 1 / i;
 		}
 		return number;
 	}
+
 	public static void main(String[] args) {
 		int n = 3;
 		System.out.println("The value of Harmonic number is:" + nthHarmonic(n));
